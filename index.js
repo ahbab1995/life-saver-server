f11fb70b17bb4df756e36b68eda31aeab2323373
ahbab1995 <islamahbab1996@yahoo.com>
2023-10-09 00:47:17 +0600

    user update in db new code added

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-
-
-
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.bijf7ad.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,30 +18,50 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+   
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("DB successfully connected ");
+    
+    const productCollection = client.db("life-saver").collection("products");
+    const userCollection = client.db("life-saver").collection("users");
+
+    
+    app.put("/user/:email", async(req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email:email };
+      const options = { upsert: true };
+      // create a document that sets the plot of the movie
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      
+      res.send(result)
+    });
+
+      
+      app.get("/product", async (req, res) => {
+        const query = {};
+        const cursor = productCollection.find(query);
+        const products = await cursor.toArray();
+        res.send(products);
+      });
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
+  console.log(`Example app listening on port ${port}`);
+});
