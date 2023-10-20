dbcbd59485234ceb795f077ee811294f5a149534
ahbab1995 <islamahbab1996@yahoo.com>
2023-10-21 02:08:23 +0600

    user admin api new code added

--- index.js
+++ index.js
@@ -21,6 +21,21 @@ const client = new MongoClient(uri, {
   },
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
    
@@ -30,6 +45,29 @@ async function run() {
     const userCollection = client.db("life-saver").collection("users");
 
     
+   
+
+    app.get('/admin/:email', async(req, res) =>{
+      const email = req.params.email;
+      const user = await userCollection.findOne({email: email});
+      const isAdmin = user.role === 'admin';
+      res.send({admin: isAdmin})
+    })
+
+    app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      
+        const updateDoc = {
+          $set: { role: 'admin' },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+    
+
+    })
+
+
     app.put("/user/:email", async(req, res) => {
       const email = req.params.email;
       const user = req.body;
@@ -43,6 +81,10 @@ async function run() {
       res.send({result,token})
     });
 
+    app.get('/user', verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
       
       app.get("/product", async (req, res) => {
         const query = {};
