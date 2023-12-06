4683535c9444789a283896ab8e78cd7d28c1dd18
ahbab1995 <islamahbab1996@yahoo.com>
2023-12-06 19:21:11 +0600

    payment system routes new code

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
-const jwt = require('jsonwebtoken');
-
+const jwt = require("jsonwebtoken");
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,12 +25,12 @@ const client = new MongoClient(uri, {
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(401).send({ message: 'UnAuthorized access' });
+    return res.status(401).send({ message: "UnAuthorized access" });
   }
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
     if (err) {
-      return res.status(403).send({ message: 'Forbidden access' })
+      return res.status(403).send({ message: "Forbidden access" });
     }
     req.decoded = decoded;
     next();
@@ -39,46 +39,51 @@ function verifyJWT(req, res, next) {
 
 async function run() {
   try {
-   
     await client.connect();
-    
+
     const productCollection = client.db("life-saver").collection("products");
-    const addproductsCollection = client.db("life-saver").collection("addproducts");
+    const addproductsCollection = client
+      .db("life-saver")
+      .collection("addproducts");
     const userCollection = client.db("life-saver").collection("users");
 
-    
     const verifyAdmin = async (req, res, next) => {
-      const requester = req.decoded.email;
-      const requesterAccount = await userCollection.findOne({ email: requester });
-      if (requesterAccount.role === 'admin') {
-        next();
+      const decodedEmail = req.decoded.email;
+      console.log("decodedEmail", decodedEmail);
+      const query = { email: decodedEmail };
+      const requesterAccount = await userCollection.findOne(query);
+      //  console.log('requesterAccount',requesterAccount)
+      if (requesterAccount?.role === "admin") {
+        return res.status(403).send({ message: "forbidden access" });
       }
-      else {
-        res.status(403).send({ message: 'forbidden' });
-      }
-    }
-   
+      next();
+    };
 
-    app.get('/admin/:email', async(req, res) =>{
-      const email = req.params.email;
-      const user = await userCollection.findOne({email: email});
-      const isAdmin = user.role === 'admin';
-      res.send({admin: isAdmin})
-    })
 
-    app.put('/user/admin/:email', verifyJWT,verifyAdmin, async (req, res) => {
+    app.post('/create-payment-intent', verifyJWT, async(req, res) =>{
+      const service = req.body;
+      const price = service.price;
+      const amount = price*100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount : amount,
+        currency: 'usd',
+        payment_method_types:['card']
+      });
+      res.send({clientSecret: paymentIntent.client_secret})
+    });
+
+    
+    app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
       const filter = { email: email };
       const updateDoc = {
-        $set: { role: 'admin' },
+        $set: { role: "admin" },
       };
       const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
+    });
 
-    })
-
-
-    app.put("/user/:email", async(req, res) => {
+    app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
       const filter = { email: email };
@@ -87,49 +92,58 @@ async function run() {
         $set: user,
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' });
-      res.send({result,token})
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "10h" }
+      );
+      res.send({ result, token });
     });
 
-    app.get('/user', verifyJWT, async (req, res) => {
+    app.get("/user", verifyJWT, async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
     });
 
-    app.delete('/user/:email', verifyJWT, verifyAdmin, async (req, res) => {
+    app.get("/admin/:email", async (req, res) => {
       const email = req.params.email;
-      const filter = {email: email};
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user?.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
+    app.delete("/user/:email", verifyJWT, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
       const result = await userCollection.deleteOne(filter);
       res.send(result);
-    })
-      
-      app.get("/product", async (req, res) => {
-        const query = {};
-        const cursor = productCollection.find(query).project({name:1});
-        const products = await cursor.toArray();
-        res.send(products);
-      });
-    
-      app.post('/addproduct', verifyJWT, verifyAdmin, async (req, res) => {
-        const product = req.body;
-        const result = await addproductsCollection.insertOne(product);
-        res.send(result);
-      });
-    
-      app.get('/addproduct',  async(req, res) =>{
-        const product = await addproductsCollection.find().toArray();
-        res.send(product);
-      })
-    
-      app.delete('/addproduct/:id', verifyJWT, verifyAdmin, async (req, res) => {
-        const id = req.params;
-        const filter = { _id: new ObjectId(id) };
-        console.log(filter)
-        const result = await addproductsCollection.deleteOne(filter);
-        res.send(result);
-      })
-    
-    
+    });
+
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query).project({ name: 1 });
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    app.post("/addproduct", verifyJWT, verifyAdmin, async (req, res) => {
+      const product = req.body;
+      const result = await addproductsCollection.insertOne(product);
+      res.send(result);
+    });
+
+    app.get("/addproduct", async (req, res) => {
+      const product = await addproductsCollection.find().toArray();
+      res.send(product);
+    });
+
+    app.delete("/addproduct/:id", verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params;
+      const filter = { _id: new ObjectId(id) };
+      console.log(filter);
+      const result = await addproductsCollection.deleteOne(filter);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -144,4 +158,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
